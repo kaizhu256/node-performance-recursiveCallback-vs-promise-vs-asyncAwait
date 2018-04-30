9e2ed6362f260d0101c83fc4afbc34d131412021
kai zhu <kaizhu256@gmail.com>
2018-04-30 16:30:09 +0800

    npm test

---
+++ test.js
@@ -0,0 +1,28 @@
+/* istanbul instrument in package node_performance_recursiveCallback_vs_promise_vs_asyncAwait */
+/* jslint-utility2 */
+/*jslint
+    bitwise: true,
+    browser: true,
+    maxerr: 4,
+    maxlen: 100,
+    node: true,
+    nomen: true,
+    regexp: true,
+    stupid: true
+*/
+'use strict';
+(function () {
+    switch (process.version.split('.')[0]) {
+    case 'v1':
+    case 'v2':
+    case 'v3':
+    case 'v4':
+    case 'v5':
+    case 'v6':
+    case 'v7':
+        require('./example.legacyjs');
+        break;
+    default:
+        require('./example.js');
+    }
+}());
